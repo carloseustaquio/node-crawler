661841decf31c6abbe4c508819ea2354f435bdb2
Carlos Eustáquio <caeu227@gmail.com>
2020-08-16 18:00:33 -0300

    change way of getting array of links in MLProductList

--- src/models/MLProductList.ts
+++ src/models/MLProductList.ts
@@ -5,19 +5,17 @@ export default class MLProductList implements ProductList {
   constructor(public limit: number) { }
 
   async crawlerRun($: CheerioSelector, url: string) {
+    console.log("Crawling products list page...")
     const searchResults = $(
       "#searchResults .item__info-link, .item__info-title"
     );
 
     this.checkResults(searchResults.length, url)
 
-    const arrLinks: Array<string | undefined> = [];
-
-    searchResults.each((index, element) => {
-      if (index < this.limit) {
-        arrLinks[index] = $(element).attr("href");
-      }
-    });
+    const arrLinks = searchResults
+      .toArray()
+      .splice(0, this.limit)
+      .map((element, index) => element.attribs.href)
 
     return arrLinks;
   }
@@ -25,9 +23,7 @@ export default class MLProductList implements ProductList {
   checkResults(length: number, url: string) {
     if (!length) {
       const sanitizedUrl = url.replace(/^https:\/\//, "").split("/")[0]
-      throw new Error(`
-        The website <b>${sanitizedUrl}</b> didn't responded, please try again.
-      `);
+      throw new Error(`The website <b>${sanitizedUrl}</b> didn't respond, please try again.`);
     }
   }
 }

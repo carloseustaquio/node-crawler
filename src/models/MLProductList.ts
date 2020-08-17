2d954cd35623a54e9b66c89415ced7b7867b8782
Carlos Eustáquio <caeu227@gmail.com>
2020-08-17 09:10:12 -0300

    adjust selectors of product list pages

--- src/models/MLProductList.ts
+++ src/models/MLProductList.ts
@@ -1,29 +1,25 @@
-import Page from "../appTypes/Page";
 import ProductList from "../appTypes/ProductList";
 
 export default class MLProductList implements ProductList {
   constructor(public limit: number) { }
 
-  async crawlerRun($: CheerioSelector, url: string) {
+  async crawlerRun($: CheerioStatic, url: string) {
     console.log("Crawling products list page...")
-    const searchResults = $(
-      "#searchResults .item__info-link, .item__info-title"
-    );
+    const searchResults = $("#searchResults, section.ui-search-results")
+    const linksOfItems = searchResults.find(".item__info-title, .item_info-title, .item-link, .ui-search-link")
 
-    this.checkResults(searchResults.length, url)
-
-    const arrLinks = searchResults
+    const arrLinks = linksOfItems
       .toArray()
       .splice(0, this.limit)
       .map((element, index) => element.attribs.href)
 
-    return arrLinks;
-  }
-
-  checkResults(length: number, url: string) {
-    if (!length) {
-      const sanitizedUrl = url.replace(/^https:\/\//, "").split("/")[0]
-      throw new Error(`The website <b>${sanitizedUrl}</b> didn't respond, please try again.`);
+    if (!arrLinks.length) {
+      throw new Error(
+        `No items returned in search. 
+        Please, try again later or with other <b>search</b> value.`
+      )
     }
+
+    return arrLinks;
   }
 }
